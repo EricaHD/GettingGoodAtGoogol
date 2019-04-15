735e7f2dd1d2d7048bedd25c409ed5acfbf24383
Isaac <isaachaberman@gmail.com>
2019-04-15 16:13:23 -0400

    Quality Control Check

--- game.py
+++ game.py
@@ -93,7 +93,7 @@ class Game():
                 if (action == 0) or (state == self.params['n_states']-1):
                     #Check for win
                     val_rank = np.where(self.params['val'] == self.states_sorted)[0][0]
-                    reward, win = self.reward_fn(val_rank)
+                    self.params['reward'], win = self.reward_fn(val_rank)
                     break
                 else:
                     self.params['reward'] = 0
