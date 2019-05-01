92142185c7e2dc78708da00be9dd21078d9219ed
EricaHD <ehd255@nyu.edu>
2019-04-30 21:46:00 -0400

    Added dropout layer to DQN

--- networks.py
+++ networks.py
@@ -12,10 +12,11 @@ class BasicDQN(nn.Module):
         super(BasicDQN, self).__init__()
         
         self.hid = nn.Linear(inp_size, hid_size)
+        self.drop = nn.Dropout(p=0.3)
         self.out = nn.Linear(hid_size, out_size)
         
     def forward(self, x):
         hid = F.relu(self.hid(x))
-        
-        out = self.out(hid)
+        dropped = self.drop(hid)
+        out = self.out(dropped)
         return F.softmax(out, dim=-1)
