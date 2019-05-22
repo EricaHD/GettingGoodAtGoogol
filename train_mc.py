45210d5da07960b6da8607d3a2bc6f736003f894
Erica Dominic <EricaHD@users.noreply.github.com>
2019-05-22 12:17:43 -0400

    Adjusted comments

--- train_mc.py
+++ train_mc.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
 
     ap = ArgumentParser()
     
-    # Agent Parameters
+    # Agent parameters
     ap.add_argument("-g", "--gamma", type=float, default=0.9,
                     help="discount factor")
     ap.add_argument("-e", "--epsilon", type=float, default=0.1,
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     ap.add_argument("-vf", "--v_fn", type=str, default="vIdx",
                     help="can be vMax or vSeq or vIdx")
     
-    # Training Game Parameters
+    # Training game parameters
     ap.add_argument("-lo", "--lo", type=int, default=1,
                     help="lowest value possible in training games")
     ap.add_argument("-hi", "--hi", type=int, default=100000,
@@ -42,7 +42,7 @@ if __name__ == '__main__':
     ap.add_argument("-rps", "--reward", type=str, default="10_10_7",
                     help="#_# when reward_fn is scalar, #_#_# when reward_fn is topN")
     
-    # Training Parameters
+    # Training parameters
     ap.add_argument("-ne", "--n_episodes", type=int, default=500000,
                     help="number of Monte Carlo episodes [mc only]")
     ap.add_argument("-np", "--n_print", type=int, default=10000,
@@ -54,7 +54,7 @@ if __name__ == '__main__':
     ap.add_argument("-crp", "--curr_params", type=str, default="0_0_10_-",
                     help="curriculum parameters, #_#_op when reward_fn is scalar, #_#_#_op when reward_fn is topN")    
     
-    # Evaluation Game Parameters
+    # Evaluation game parameters
     ap.add_argument("-loe", "--lo_eval", type=int, default=1,
                     help="lowest value possible in evaluation games")
     ap.add_argument("-hie", "--hi_eval", type=int, default=100000,
@@ -68,7 +68,7 @@ if __name__ == '__main__':
     ap.add_argument("-rpse", "--reward_eval", type=str, default="1_1",
                     help="#_# when reward_fn_eval is scalar, #_#_# when reward_fn_eval is topN")
     
-    # Evaluation Parameters
+    # Evaluation parameters
     ap.add_argument("-nge", "--n_games_eval", type=int, default=10000,
                     help="number of evaluation games")
     ap.add_argument("-npe", "--n_print_eval", type=int, default=1000,
@@ -76,7 +76,7 @@ if __name__ == '__main__':
     ap.add_argument("-de", "--delay_eval", type=int, default=0,
                     help="time delay in evaluation games")
     
-    # Save Path
+    # Save path
     ap.add_argument("-fp", "--file_path",
                     help="file path used for saving")
     ap.add_argument("-scfp", "--sc_file_path",
